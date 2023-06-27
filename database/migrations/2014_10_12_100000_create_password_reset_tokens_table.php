a77f8759362ef6fc7c7c3065694bbe22b3c37b7c
progRizvi <prog.rizvi@gmail.com>
2023-06-27 13:34:54 +0600

    password_reset_token

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->string('email', 255);
+            $table->bigIncrements('id');
+            $table->string('email', 255)->unique();
             $table->string('token');
             $table->timestamps();
         });
+
     }
 
     /**
