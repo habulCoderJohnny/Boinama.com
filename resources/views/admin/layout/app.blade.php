c8ee3d6bc4aff8ce0f3fb45ed12beccec3969168
progRizvi <prog.rizvi@gmail.com>
2023-06-27 15:30:06 +0600

    design fixed

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -12,12 +12,12 @@
     <!-- Font Awesome Icons -->
     <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
     <!-- Nucleo Icons -->
-    <link href="{{ url('admin/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-    <link href="{{ url('admin/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ url('public/admin/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ url('public/admin/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- Popper -->
     <script src="https://unpkg.com/@popperjs/core@2"></script>
     <!-- Main Styling -->
-    <link href="{{ url('admin/assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5') }}" rel="stylesheet" />
+    <link href="{{ url('public/admin/assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5') }}" rel="stylesheet" />
     <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
     <script defer data-site="YOUR_DOMAIN_HERE" src="https://api.nepcha.com/js/nepcha-analytics.js"></script>
@@ -41,13 +41,13 @@
     </main>
 </body>
 <!-- plugin for charts  -->
-<script src="{{ url('admin/assets/js/plugins/chartjs.min.js') }}" async></script>
+<script src="{{ url('public/admin/assets/js/plugins/chartjs.min.js') }}" async></script>
 <!-- plugin for scrollbar  -->
-<script src="{{ url('admin/assets/js/plugins/perfect-scrollbar.min.js') }}" async></script>
+<script src="{{ url('public/admin/assets/js/plugins/perfect-scrollbar.min.js') }}" async></script>
 <!-- github button -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <!-- main script file  -->
-<script src="{{ url('admin/assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5') }}" async></script>
+<script src="{{ url('public/admin/assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5') }}" async></script>
 @stack('scripts')
 
 </html>
